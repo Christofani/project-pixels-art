2e1a5e01d9c0d2d19c791d2564eb034c4dc74cf0
Rodrigo Christofani <christofanir@gmail.com>
2023-05-18 15:31:11 -0300

    Resolução requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable sonarjs/no-unused-collection */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 const titulo = document.querySelector('h1');
@@ -27,6 +29,29 @@ const coresAleatorias = () => {
   return cor;
 };
 
+const SalvaCores = () => {
+  // eslint-disable-next-line no-shadow
+  const cores = [];
+  const colorDivs = document.querySelectorAll('.color');
+  colorDivs.forEach((color) => {
+    const cor = color.style.backgroundColor;
+    cores.push(cor);
+  });
+  localStorage.setItem('colorPalette', JSON.stringify(cores));
+};
+
+const coresLocalStorage = () => {
+  const coresSalvas = localStorage.getItem('colorPalette');
+  if (coresSalvas) {
+    const cores = JSON.parse(coresSalvas);
+    const divs = document.querySelectorAll('.color');
+    divs.forEach((div, index) => {
+      div.style.backgroundColor = cores[index];
+    });
+  }
+};
+coresLocalStorage();
+
 const botao = document.querySelector('#button-random-color');
 botao.addEventListener('click', () => {
   const elementoDiv = document.querySelectorAll('.color');
@@ -36,4 +61,5 @@ botao.addEventListener('click', () => {
       div.style.backgroundColor = corNova;
     }
   });
+  SalvaCores();
 });
