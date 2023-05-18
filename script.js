dd50f36aca0f0340bed03b2d4ee1c56bf7f582be
Rodrigo Christofani <christofanir@gmail.com>
2023-05-17 22:35:58 -0300

    Resolução requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-unused-vars */
 const titulo = document.querySelector('h1');
 titulo.id = 'title';
 titulo.innerText = 'Paleta de Cores';
+
+const elementoPai = document.querySelector('section');
+elementoPai.id = 'color-palette';
+const cores = ['red', 'blue', 'green', 'yellow'];
+
+const criaElementos = () => {
+  for (let index = 0; index < 4; index += 1) {
+    const divElement = document.createElement('div');
+    divElement.classList.add('color');
+    divElement.style.backgroundColor = cores[index];
+    elementoPai.appendChild(divElement);
+  }
+};
+criaElementos();
