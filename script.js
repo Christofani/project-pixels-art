7fcf23113f8d5efc8a57c0e30078fa5bc2a2eed3
Rodrigo Christofani <christofanir@gmail.com>
2023-05-19 00:58:12 -0300

    Resolução requisito 7

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const cores = ['black', 'blue', 'purple', 'yellow'];
 const criaElementos = () => {
   for (let index = 0; index < 4; index += 1) {
     const divElement = document.createElement('div');
+    if (cores[index] === 'black') {
+      divElement.className = 'selected';
+    }
     divElement.classList.add('color');
     divElement.style.backgroundColor = cores[index];
     elementoPai.appendChild(divElement);
@@ -30,24 +33,25 @@ const coresAleatorias = () => {
 };
 
 const SalvaCores = () => {
-  // eslint-disable-next-line no-shadow
   const cores = [];
   const colorDivs = document.querySelectorAll('.color');
-  colorDivs.forEach((color) => {
+  for (let index = 0; index < colorDivs.length; index += 1) {
+    const color = colorDivs[index];
     const cor = color.style.backgroundColor;
     cores.push(cor);
-  });
+  }
   localStorage.setItem('colorPalette', JSON.stringify(cores));
 };
 
 const coresLocalStorage = () => {
   const coresSalvas = localStorage.getItem('colorPalette');
   if (coresSalvas) {
-    const cores = JSON.parse(coresSalvas);
+    const coresAl = JSON.parse(coresSalvas);
     const divs = document.querySelectorAll('.color');
-    divs.forEach((div, index) => {
-      div.style.backgroundColor = cores[index];
-    });
+    for (let index = 0; index < divs.length; index += 1) {
+      const div = divs[index];
+      div.style.backgroundColor = coresAl[index];
+    }
   }
 };
 coresLocalStorage();
@@ -55,11 +59,30 @@ coresLocalStorage();
 const botao = document.querySelector('#button-random-color');
 botao.addEventListener('click', () => {
   const elementoDiv = document.querySelectorAll('.color');
-  elementoDiv.forEach((div, posicao) => {
-    if (posicao !== 0) {
-      const corNova = coresAleatorias();
-      div.style.backgroundColor = corNova;
-    }
-  });
+  for (let index = 1; index < elementoDiv.length; index += 1) {
+    const div = elementoDiv[index];
+    const corNova = coresAleatorias();
+    div.style.backgroundColor = corNova;
+  }
   SalvaCores();
 });
+
+const paiElemento = document.querySelectorAll('section')[1];
+paiElemento.id = 'pixel-board';
+
+const criaPixels = () => {
+  const bloco = document.createElement('div');
+  bloco.classList.add('bloco');
+  paiElemento.appendChild(bloco);
+  for (let index = 0; index < 5; index += 1) {
+    const linhaindex = document.createElement('div');
+    linhaindex.classList.add('linha');
+    bloco.appendChild(linhaindex);
+    for (let index = 0; index < 5; index += 1) {
+      const Blocoindex = document.createElement('div');
+      Blocoindex.classList.add('pixel');
+      linhaindex.appendChild(Blocoindex);
+    }
+  }
+};
+criaPixels();
